b0bd61f17c962bf50352d4c2d4f6d0872b831757
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-05-15 19:11:57 +0530

    add bacic styles to home>shortstatus section

--- habitapp/src/components/home/ShortStatus.tsx
+++ habitapp/src/components/home/ShortStatus.tsx
@@ -3,11 +3,14 @@ import { Text, View, StyleSheet } from 'react-native'
 import { useUserStore } from '../../store/UserStore'
 import PagerView from 'react-native-pager-view';
 import { addDays, eachDayOfInterval, eachWeekOfInterval, format, subDays } from 'date-fns';
+import { AnimatedCircularProgress } from 'react-native-circular-progress';
+import useColorStore from '../../store/ColorStore';
 
 const ShortStatus = () => {
   const user = useUserStore(state => state.user)
   const [today] = useState(new Date())
-
+  const currentTheme = useColorStore(state => state.currentTheme);
+  const primaryColors = useColorStore(state => state.primaryColors);
 
   const dates = eachWeekOfInterval(
     {
@@ -15,7 +18,7 @@ const ShortStatus = () => {
       end: addDays(new Date(), 14)
     },
     {
-      weekStartsOn:1
+      weekStartsOn: 1
     }
   ).reduce((acc: Date[][], cur) => {
     const alldays = eachDayOfInterval({
@@ -29,30 +32,50 @@ const ShortStatus = () => {
   }, [])
 
   return (
-    <View style={{ height: 100 }}>
-      <View >
-        <Text >
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={[styles.dateText, { color: currentTheme.SecondoryText }]}>
           {today.toLocaleDateString('en-GB', {
             day: '2-digit',
             month: 'long',
             year: 'numeric',
           })}
         </Text>
-        <Text >Hi, {user?.name}</Text>
+        <Text style={[styles.welcomeText, { color: currentTheme.PrimaryText }]}>Hi, {user?.name}</Text>
       </View>
 
       <PagerView style={styles.pagerView} initialPage={0}>
         {
           dates.map((week, i) => {
             return (
-              <View key={i}>
-                <View style={{ flexDirection: "row",justifyContent:"space-around" ,alignItems:"center"}}>
-                  {week.map((day) => {
+              <View key={i} style={styles.weekContainer}>
+                <View style={styles.daysRow}>
+                  {week.map((day, index) => {
                     const txt = format(day, 'EEEEE')
+                    const isToday = day.getDate() === today.getDate() && 
+                                   day.getMonth() === today.getMonth() &&
+                                   day.getFullYear() === today.getFullYear();
+                    
                     return (
-                      <View>
-                        <Text>{txt}</Text>
-                         <Text>{day.getDate()}</Text>
+                      <View 
+                        key={index} 
+                        style={[
+                          styles.dayContainer, 
+                          isToday && { backgroundColor: primaryColors.Primary, borderRadius: 20 }
+                        ]}
+                      >
+                        <Text style={[
+                          styles.dayText, 
+                          { color: isToday ? currentTheme.ButtonText : currentTheme.PrimaryText }
+                        ]}>
+                          {txt}
+                        </Text>
+                        <Text style={[
+                          styles.dateNumber, 
+                          { color: isToday ? currentTheme.ButtonText : currentTheme.PrimaryText }
+                        ]}>
+                          {day.getDate()}
+                        </Text>
                       </View>
                     )
                   })}
@@ -63,14 +86,99 @@ const ShortStatus = () => {
         }
       </PagerView>
 
+      <View style={[styles.statsContainer, { backgroundColor: currentTheme.Card }]}>
+        <AnimatedCircularProgress
+          size={120}
+          width={15}
+          fill={60}
+          rotation={0}
+          tintColor={primaryColors.Primary}
+          onAnimationComplete={() => console.log('onAnimationComplete')}
+          backgroundColor={currentTheme.Border} />
+          <View style={styles.statsTextContainer}>
+            <Text style={[styles.statsTitle, { color: currentTheme.PrimaryText }]}>Daily Habits</Text>
+            <Text style={[styles.statsSubtitle, { color: primaryColors.Accent }]}>Almost there! Goals in reach!</Text>
+            <Text style={[styles.statsProgress, { color: currentTheme.SecondoryText }]}>3/7 of habit complete</Text>
+          </View>
+      </View>
+
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    height: 500,
+    padding: 16,
+  },
+  headerContainer: {
+    marginBottom: 16,
+  },
+  dateText: {
+    fontSize: 14,
+    marginBottom: 4,
+  },
+  welcomeText: {
+    fontSize: 24,
+    fontWeight: '700',
+    marginBottom: 16,
+  },
   pagerView: {
     flex: 1,
+    marginBottom: 16,
   },
+  weekContainer: {
+    paddingVertical: 12,
+  },
+  daysRow: {
+    flexDirection: "row", 
+    justifyContent: "space-around", 
+    alignItems: "center",
+  },
+  dayContainer: {
+    width: 40,
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 8,
+  },
+  dayText: {
+    fontSize: 12,
+    fontWeight: '600',
+    marginBottom: 4,
+  },
+  dateNumber: {
+    fontSize: 16,
+    fontWeight: '700',
+  },
+  statsContainer: {
+    flexDirection: 'row',
+    padding: 16,
+    borderRadius: 20,
+    alignItems: 'center',
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    elevation: 4,
+  },
+  statsTextContainer: {
+    flex: 1,
+    marginLeft: 20,
+  },
+  statsTitle: {
+    fontSize: 18,
+    fontWeight: '700',
+    marginBottom: 4,
+  },
+  statsSubtitle: {
+    fontSize: 14,
+    fontWeight: '600',
+    marginBottom: 4,
+  },
+  statsProgress: {
+    fontSize: 14,
+  }
 });
 
 export default ShortStatus
--- habitapp/src/screens/Home.screen.tsx
+++ habitapp/src/screens/Home.screen.tsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import { FlatList, Text, View } from 'react-native'
+import { FlatList, ScrollView, Text, View } from 'react-native'
 import { useUserStore } from '../store/UserStore';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useHabitStore } from '../store/HabitsStore';
 import ShortStatus from '../components/home/ShortStatus';
+import useColorStore from '../store/ColorStore';
 
 const Home = () => {
+
+  const currentTheme = useColorStore(state => state.currentTheme);
+  const primaryColors = useColorStore(state => state.primaryColors);
 
   const user = useUserStore(state => state.user)
   const loadHabits = useHabitStore(state => state.loadHabits)
@@ -20,39 +24,41 @@ const Home = () => {
   const [today] = useState(new Date())
 
   return (
-    <SafeAreaView>
-      <ShortStatus />
-      <FlatList
-        data={habits}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <View>
-            <Text>-------------------------</Text>
-            <Text>{item.id}</Text>
-            <Text>{item.userId}</Text>
-            <Text>{item.name}</Text>
-            <Text>{item.description}</Text>
-            <Text>{item.endDate ? item.endDate.toString() : 'No end date'}</Text>
-            <Text>
-              {item.goal
-                ? item.goal.type === 'units'
-                  ? `Units: ${item.goal.amount}`
-                  : `Timer: ${item.goal.timePeriod.hours}h ${item.goal.timePeriod.minutes}m`
-                : 'No goal'}
-            </Text>
-            <Text>{item.reminder ? item.reminder.toString() : 'No reminder'}</Text>
-            <Text>
-              {item.repeat.type === 'daily'
-                ? `Daily: ${item.repeat.days.join(', ')}`
-                : item.repeat.type === 'weekly'
-                  ? 'Weekly'
-                  : 'Monthly'}
-            </Text>
-          </View>
-        )}
-        ListEmptyComponent={<Text style={{ textAlign: 'center', marginTop: 20 }}>No habits found</Text>}
+    <SafeAreaView style={{ backgroundColor: currentTheme.Background }}>
+      <ScrollView>
+        <ShortStatus />
+        <FlatList
+          data={habits}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <View>
+              <Text>-------------------------</Text>
+              <Text>{item.id}</Text>
+              <Text>{item.userId}</Text>
+              <Text>{item.name}</Text>
+              <Text>{item.description}</Text>
+              <Text>{item.endDate ? item.endDate.toString() : 'No end date'}</Text>
+              <Text>
+                {item.goal
+                  ? item.goal.type === 'units'
+                    ? `Units: ${item.goal.amount}`
+                    : `Timer: ${item.goal.timePeriod.hours}h ${item.goal.timePeriod.minutes}m`
+                  : 'No goal'}
+              </Text>
+              <Text>{item.reminder ? item.reminder.toString() : 'No reminder'}</Text>
+              <Text>
+                {item.repeat.type === 'daily'
+                  ? `Daily: ${item.repeat.days.join(', ')}`
+                  : item.repeat.type === 'weekly'
+                    ? 'Weekly'
+                    : 'Monthly'}
+              </Text>
+            </View>
+          )}
+          ListEmptyComponent={<Text style={{ textAlign: 'center', marginTop: 20 }}>No habits found</Text>}
 
-      />
+        />
+      </ScrollView>
     </SafeAreaView>
   )
 }
